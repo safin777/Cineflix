1711c9cfd6b234993dfe88b72634eb9018f02fca
safin777 <sayedislamsafin7770555@gmail.com>
2021-11-20 02:08:18 +0600

    Project done ...

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
+
+//slider
 const arrows = document.querySelectorAll(".arrow");
 const movieLists = document.querySelectorAll(".movie-list");
 
 arrows.forEach((arrow,index) => {
+  const ratio = Math.floor(window.innerWidth / 270);
   const itemNumber = movieLists[index].querySelectorAll("img").length;
   let clickCounter = 0;
 
   arrow.addEventListener("click", ()=>{
     clickCounter++;
-    if (itemNumber - (4 + clickCounter) > 0){
+    if (itemNumber - (4 + clickCounter) + (4 - ratio) > 0){
       movieLists[index].style.transform = `translateX(${movieLists[index].computedStyleMap().get("transform")[0].x.value-300}px)`;
     }else{
       movieLists[index].style.transform = "translateX(0)";
       clickCounter = 0;
     }
   }) 
+  console.log(Math.floor(window.innerWidth / 270));
 
-})
+});
 
-
+//toggle
 
 const ball = document.querySelector(".toggle-ball");
 const items = document.querySelectorAll(".toggle , .movie-list-title , .navbar-container , .sidebar , .toggle , .movie-list-container");
