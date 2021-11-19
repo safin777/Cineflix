454ec0e269873c916efb7128d981dd46163559df
safin777 <sayedislamsafin7770555@gmail.com>
2021-11-20 00:09:47 +0600

    slider completed

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ const arrows = document.querySelectorAll(".arrow");
 const movieLists = document.querySelectorAll(".movie-list");
 
 arrows.forEach((arrow,index) => {
+  const itemNumber = movieLists[index].querySelectorAll("img").length;
+  let clickCounter = 0;
+
   arrow.addEventListener("click", ()=>{
-    movieLists[index].style.transform = `translateX(${movieLists[index].computedStyleMap().get("transform")[0].x.value-300}px)`;
+    clickCounter++;
+    if (itemNumber - (4 + clickCounter) > 0){
+      movieLists[index].style.transform = `translateX(${movieLists[index].computedStyleMap().get("transform")[0].x.value-300}px)`;
+    }else{
+      movieLists[index].style.transform = "translateX(0)";
+      clickCounter = 0;
+    }
   }) 
 
 })
