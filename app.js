0197279e001f5243f9b1c822121836ab423827cb
safin777 <sayedislamsafin7770555@gmail.com>
2021-11-19 23:33:59 +0600

    arrow slider made successfully

---
+++ app.js
@@ -0,0 +1,9 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow,index) => {
+  arrow.addEventListener("click", ()=>{
+    movieLists[index].style.transform = `translateX(${movieLists[index].computedStyleMap().get("transform")[0].x.value-300}px)`;
+  }) 
+
+})
